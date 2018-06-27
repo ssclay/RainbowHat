9d21b55210f77e309b888cb28dbd569ecf24a1d9
Scott Clay <sclay@n.io>
2018-06-27 11:33:19 -0600

    floats or words

--- AlphaDisplay_block.py
+++ AlphaDisplay_block.py
@@ -7,10 +7,20 @@ class AlphaDisplay(Block):
 
     version = VersionProperty('0.1.0')
     words = Property(title="Words to Screen", default='AHOY')
+    floats = Property(title="Floats to Screen", default='1337')
 
     def process_signals(self, signals):
-        for signal in signals:
-            rh.display.clear()
-            rh.display.print_str(self.words())
-            rh.display.show()
-        self.notify_signals(signals)
+        
+	    if self.words():
+	        for signal in signals:
+	            rh.display.clear()
+	            rh.display.print_str(self.words(signal))
+	            rh.display.show()
+	        self.notify_signals(signals)
+
+	    if self.floats():
+	        for signal in signals:
+	            rh.display.clear()
+	            rh.display.print_float(self.floats(signal))
+	            rh.display.show()
+	        self.notify_signals(signals)
--- tests/test_AlphaDisplay_block.py
+++ tests/test_AlphaDisplay_block.py
@@ -15,15 +15,26 @@ class TestAlphaDisplay(NIOBlockTestCase):
         global AlphaDisplay
 
     def test_string(self):
-        """Signals pass through block unmodified."""
         with patch(AlphaDisplay.__module__ +'.rh.display') as mock_display:
             blk = AlphaDisplay()
             self.configure_block(blk, {'words':'HIII'})
             blk.start()
             blk.process_signals([Signal({})])
             blk.stop()
-            self.assert_num_signals_notified(1)
             mock_display.print_str.assert_called_with('HIII')
+            mock_display.show.assert_called()
+            self.assertDictEqual(
+                self.last_notified[DEFAULT_TERMINAL][0].to_dict(),
+                {})
+
+    def test_floats(self):
+        with patch(AlphaDisplay.__module__ +'.rh.display') as mock_display:
+            blk = AlphaDisplay()
+            self.configure_block(blk, {'floats':'8008'})
+            blk.start()
+            blk.process_signals([Signal({})])
+            blk.stop()
+            mock_display.print_float.assert_called_with('8008')
             mock_display.show.assert_called()
             self.assertDictEqual(
                 self.last_notified[DEFAULT_TERMINAL][0].to_dict(),
