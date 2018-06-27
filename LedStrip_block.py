855b90e3bb9c156d95dda0e9b7a69fd38fb3ba1e
Scott Clay <sclay@n.io>
2018-06-27 15:40:41 -0600

    need to extrapolate

--- LedStrip_block.py
+++ LedStrip_block.py
@@ -6,9 +6,22 @@ import rainbowhat as rh
 class LedStrip(Block):
 
     version = VersionProperty('0.1.0')
+    pixelrgb0 = Property(title='pixel 0 r g b', default=(0,0,0))
+    pixelrgb1 = Property(title='pixel 1 r g b', default=(0,0,0))
+    pixelrgb2 = Property(title='pixel 2 r g b', default=(0,0,0))
+    pixelrgb3 = Property(title='pixel 3 r g b', default=(0,0,0))
+    pixelrgb4 = Property(title='pixel 4 r g b', default=(0,0,0))
+    pixelrgb5 = Property(title='pixel 5 r g b', default=(0,0,0))
+    pixelrgb6 = Property(title='pixel 6 r g b', default=(0,0,0))
+    pixelrgb7 = Property(title='pixel 7 r g b', default=(0,0,0))
 
     def process_signals(self, signals):
-        
+
         for signal in signals:
-            pass
+            rh.rainbow.set_pixel(0, 
+                        self.pixelrgb0(signal)[0], 
+                        self.pixelrgb0(signal)[1], 
+                        self.pixelrgb0(signal)[2])
+            rh.rainbow.show()
+
         self.notify_signals(signals)
--- tests/test_LedStrip_block.py
+++ tests/test_LedStrip_block.py
@@ -14,13 +14,17 @@ class TestLedStrip(NIOBlockTestCase):
         from ..LedStrip_block import LedStrip
         global LedStrip
 
+    color = (255, 200, 150)
+
     def test_led(self):
         with patch(LedStrip.__module__ +'.rh.rainbow') as mock_led:
             blk = LedStrip()
-            self.configure_block(blk, {})
+            self.configure_block(blk, ({'pixelrgb0': self.color}))
             blk.start()
             blk.process_signals([Signal({})])
             blk.stop()
+            mock_led.set_pixel.assert_called_with(0, 255, 200, 150)
+            mock_led.show.assercalled()
             self.assertDictEqual(
                 self.last_notified[DEFAULT_TERMINAL][0].to_dict(),
                 {})
