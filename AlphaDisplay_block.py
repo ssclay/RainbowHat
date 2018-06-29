d3cef07d365e43a90f69ed5c28918d59efbd541c
Scott Clay <sclay@n.io>
2018-06-29 11:20:37 -0600

    onlystrings

--- AlphaDisplay_block.py
+++ AlphaDisplay_block.py
@@ -8,20 +8,20 @@ class AlphaDisplay(Block):
 
     version = VersionProperty('0.1.0')
     words = Property(title="Words to Screen", default=None, allow_none=True)
-    floats = Property(title="Floats to Screen", default=None, allow_none=True)
+    # floats = Property(title="Floats to Screen", default=None, allow_none=True)
 
     def process_signals(self, signals):
 
-        if self.words():
-            for signal in signals:
-                rh.display.clear()
-                rh.display.print_str(self.words(signal))
-            rh.display.show()
-            self.notify_signals(signals)
+        # if self.words():
+        for signal in signals:
+            rh.display.clear()
+            rh.display.print_str(self.words(signal))
+        rh.display.show()
+        self.notify_signals(signals)
 
-        if self.floats():
-            for signal in signals:
-                rh.display.clear()
-                rh.display.print_float(self.floats(signal))
-                rh.display.show()
-            self.notify_signals(signals)
+        # if self.floats():
+        #     for signal in signals:
+        #         rh.display.clear()
+        #         rh.display.print_float(self.floats(signal))
+        #         rh.display.show()
+        #     self.notify_signals(signals)
