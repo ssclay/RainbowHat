ed428f3f7672d7c0ff337a2168e054ed17909270
Scott Clay <sclay@n.io>
2018-06-29 13:35:56 -0600

    stringify all

--- AlphaDisplay_block.py
+++ AlphaDisplay_block.py
@@ -12,16 +12,8 @@ class AlphaDisplay(Block):
 
     def process_signals(self, signals):
 
-        # if self.words():
         for signal in signals:
             rh.display.clear()
-            rh.display.print_str(self.words(signal))
+            rh.display.print_str(str(self.words(signal)))
         rh.display.show()
         self.notify_signals(signals)
-
-        # if self.floats():
-        #     for signal in signals:
-        #         rh.display.clear()
-        #         rh.display.print_float(self.floats(signal))
-        #         rh.display.show()
-        #     self.notify_signals(signals)
--- tests/test_AlphaDisplay_block.py
+++ tests/test_AlphaDisplay_block.py
@@ -28,15 +28,15 @@ class TestAlphaDisplay(NIOBlockTestCase):
                 self.last_notified[DEFAULT_TERMINAL][0].to_dict(),
                 {})
 
-    # def test_floats(self):
-    #     with patch(AlphaDisplay.__module__ + '.rh.display') as mock_display:
-    #         blk = AlphaDisplay()
-    #         self.configure_block(blk, {'floats': '8008'})
-    #         blk.start()
-    #         blk.process_signals([Signal({})])
-    #         blk.stop()
-    #         mock_display.print_float.assert_called_with('8008')
-    #         mock_display.show.assert_called()
-    #         self.assertDictEqual(
-    #             self.last_notified[DEFAULT_TERMINAL][0].to_dict(),
-    #             {})
+    def test_floats(self):
+        with patch(AlphaDisplay.__module__ + '.rh.display') as mock_display:
+            blk = AlphaDisplay()
+            self.configure_block(blk, {'words': '800.8'})
+            blk.start()
+            blk.process_signals([Signal({})])
+            blk.stop()
+            mock_display.print_str.assert_called_with('800.8')
+            mock_display.show.assert_called()
+            self.assertDictEqual(
+                self.last_notified[DEFAULT_TERMINAL][0].to_dict(),
+                {})
