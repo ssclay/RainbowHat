0ea4d4c23ff4d89a343925e8cd3fe247d75be97e
Scott Clay <sclay@n.io>
2018-06-29 11:19:35 -0600

    new default

--- LedStrip_block.py
+++ LedStrip_block.py
@@ -7,14 +7,14 @@ import rainbowhat as rh
 class LedStrip(Block):
 
     version = VersionProperty('0.1.0')
-    pixelrgb0 = Property(title='pixel 0 r g b', default=(0, 0, 0))
-    pixelrgb1 = Property(title='pixel 1 r g b', default=(0, 0, 0))
-    pixelrgb2 = Property(title='pixel 2 r g b', default=(0, 0, 0))
-    pixelrgb3 = Property(title='pixel 3 r g b', default=(0, 0, 0))
-    pixelrgb4 = Property(title='pixel 4 r g b', default=(0, 0, 0))
-    pixelrgb5 = Property(title='pixel 5 r g b', default=(0, 0, 0))
-    pixelrgb6 = Property(title='pixel 6 r g b', default=(0, 0, 0))
-    pixelrgb7 = Property(title='pixel 7 r g b', default=(0, 0, 0))
+    pixelrgb0 = Property(title='pixel 0 r g b', default='{{(0, 0, 0)}}')
+    pixelrgb1 = Property(title='pixel 1 r g b', default='{{(0, 0, 0)}}')
+    pixelrgb2 = Property(title='pixel 2 r g b', default='{{(0, 0, 0)}}')
+    pixelrgb3 = Property(title='pixel 3 r g b', default='{{(0, 0, 0)}}')
+    pixelrgb4 = Property(title='pixel 4 r g b', default='{{(0, 0, 0)}}')
+    pixelrgb5 = Property(title='pixel 5 r g b', default='{{(0, 0, 0)}}')
+    pixelrgb6 = Property(title='pixel 6 r g b', default='{{(0, 0, 0)}}')
+    pixelrgb7 = Property(title='pixel 7 r g b', default='{{(0, 0, 0)}}')
 
     def process_signals(self, signals):
 
